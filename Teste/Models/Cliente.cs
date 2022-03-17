72dcdda27b26b7bcf82b89b4d044f8a04914dec8
ArmandoMarioto <armando_marioto100@hotmail.com>
2022-03-17 20:04:32 -0300

    Adicionar arquivos de projeto.

---
+++ Teste/Models/Cliente.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Teste.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int ClienteId { get; set; }
+
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+
+        public DateTime Date { get; set; }
+
+        public string Tipo { get; set; }
+        [Required]
+
+        public string Cpf { get; set; }
+        [Required]
+
+        public string Estadual { get; set; }
+        [Required]
+
+        public string Isento { get; set; }
+        [Required]
+
+        public string Genero { get; set; }
+
+    }
+}
